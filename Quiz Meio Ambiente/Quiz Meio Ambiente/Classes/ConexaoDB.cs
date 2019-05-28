3f852f5cc0dd9e6e4567224efa0c38c5fa45477f
Marlon Cruz <marlomsilvacruz@gmail.com>
2019-05-28 00:11:56 -0300

    Iniciando projeto

---
+++ Quiz Meio Ambiente/Quiz Meio Ambiente/Classes/ConexaoDB.cs
@@ -0,0 +1,140 @@
+﻿using Quiz_Meio_Ambiente.Properties;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz_Meio_Ambiente.Classes
+{
+    class ConexaoDB
+    {
+        private SqlConnection CriarConexao()
+        {
+            return new SqlConnection(Settings.Default.DB);
+        }
+
+        private SqlParameterCollection sqlParameterCollection = new SqlCommand().Parameters;
+
+    class DBConn
+    {
+        private SqlConnection CriarConexao()
+        {
+            return new SqlConnection(Settings.Default.DB);
+        }
+        private SqlParameterCollection sqlParameterCollection = new SqlCommand().Parameters;
+        public void LimparParametros()
+        {
+            sqlParameterCollection.Clear();
+        }
+        public void AdicionarParametros(string nomeParametro, object valorParametro)
+        {
+            sqlParameterCollection.Add(new SqlParameter(nomeParametro, valorParametro));
+        }
+        public object ExecutarManipulacao(CommandType commandType, string nomeStoredProcedureOuTextoSql)
+        {
+            try { 
+                SqlConnection sqlConnection = CriarConexao();
+                sqlConnection.Open();
+                SqlCommand sqlCommand = sqlConnection.CreateCommand();
+                sqlCommand.CommandType = commandType;
+                sqlCommand.CommandText = nomeStoredProcedureOuTextoSql;
+                sqlCommand.CommandTimeout = 999999999;
+                foreach(SqlParameter sqlParameter in sqlParameterCollection)
+                {
+                    sqlCommand.Parameters.Add(new SqlParameter(sqlParameter.ParameterName, sqlParameter.Value));
+                }
+                return sqlCommand.ExecuteScalar();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public DataTable ExecutarConsulta(CommandType commandType, string nomeStoredProcedureOuTextoSql)
+        {
+            try
+            {
+                SqlConnection sqlConnection = CriarConexao();
+                sqlConnection.Open();
+                SqlCommand sqlCommand = sqlConnection.CreateCommand();
+                sqlCommand.CommandType = commandType;
+                sqlCommand.CommandText = nomeStoredProcedureOuTextoSql;
+                sqlCommand.CommandTimeout = 999999999; 
+                foreach (SqlParameter sqlParameter in sqlParameterCollection)
+                {
+                    sqlCommand.Parameters.Add(new SqlParameter(sqlParameter.ParameterName, sqlParameter.Value));
+                }
+                SqlDataAdapter sqldataAdapter = new SqlDataAdapter(sqlCommand);
+                DataTable dataTable = new DataTable();
+                sqldataAdapter.Fill(dataTable);
+                return dataTable;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+    }
+        public void LimparParametros()
+        {
+            sqlParameterCollection.Clear();
+        }
+        public void AdicionarParametros(string nomeParametro, object valorParametro)
+        {
+            sqlParameterCollection.Add(new SqlParameter(nomeParametro, valorParametro));
+        }
+        public object ExecutarManipulacao(CommandType commandType, string nomeStoredProcedureOuTextoSql)
+        {
+            try
+            {
+                SqlConnection sqlConnection = CriarConexao();
+                sqlConnection.Open();
+                SqlCommand sqlCommand = sqlConnection.CreateCommand();
+                sqlCommand.CommandType = commandType;
+                sqlCommand.CommandText = nomeStoredProcedureOuTextoSql;
+                sqlCommand.CommandTimeout = 999999999; 
+                foreach (SqlParameter sqlParameter in sqlParameterCollection)
+                {
+                    sqlCommand.Parameters.Add(new SqlParameter(sqlParameter.ParameterName, sqlParameter.Value));
+                }
+                return sqlCommand.ExecuteScalar();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public DataTable ExecutarConsulta(CommandType commandType, string nomeStoredProcedureOuTextoSql)
+        {
+            try
+            {
+                SqlConnection sqlConnection = CriarConexao();
+                sqlConnection.Open();
+                SqlCommand sqlCommand = sqlConnection.CreateCommand();
+                sqlCommand.CommandType = commandType;
+                sqlCommand.CommandText = nomeStoredProcedureOuTextoSql;
+                sqlCommand.CommandTimeout = 999999999;
+                foreach (SqlParameter sqlParameter in sqlParameterCollection)
+                {
+                    sqlCommand.Parameters.Add(new SqlParameter(sqlParameter.ParameterName, sqlParameter.Value));
+                }
+                SqlDataAdapter sqldataAdapter = new SqlDataAdapter(sqlCommand);
+                DataTable dataTable = new DataTable();
+                sqldataAdapter.Fill(dataTable);
+
+                return dataTable;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+    }
+}
---
+++ Quiz Meio Ambiente/Quiz Meio Ambiente/FormCriarJogo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quiz_Meio_Ambiente
+{
+    public partial class FormCriarJogo : Form
+    {
+        public FormCriarJogo()
+        {
+            InitializeComponent();
+        }
+    }
+}
